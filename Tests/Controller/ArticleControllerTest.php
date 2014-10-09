1c341dc80397657c356449dfca8d3eb8caefd39f
Monika <monika@bodega>
2014-10-09 22:28:05 +0200

    Updated test

--- Tests/Controller/ArticleControllerTest.php
+++ Tests/Controller/ArticleControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ArticleControllerTest extends WebTestCase
 {
-    /*
+    
     public function testCompleteScenario()
     {
         // Create a new client to browse the application
@@ -19,37 +19,15 @@ class ArticleControllerTest extends WebTestCase
 
         // Fill in the form and submit it
         $form = $crawler->selectButton('Create')->form(array(
-            'pvessel_articlebundle_article[field_name]'  => 'Test',
-            // ... other fields to fill
+            'pvessel_articlebundle_article[title]'  => 'TestTitle',
+            'pvessel_articlebundle_article[content]'  => 'TestContent',
         ));
 
         $client->submit($form);
         $crawler = $client->followRedirect();
 
         // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Update')->form(array(
-            'pvessel_articlebundle_article[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
-
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $this->assertGreaterThan(0, $crawler->filter('td:contains("TestTitle")')->count(), 'Missing element td:contains("TestTitle")');
     }
 
-    */
 }
